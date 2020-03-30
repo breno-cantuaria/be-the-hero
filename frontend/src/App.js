9ebf5d131eaceb404399e626ed87cf4ab0328146
Breno Cantuária <breno7991@gmail.com>
2020-03-29 22:54:34 -0300

    Front-end da aplicação finalizado.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css'
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello Breno, this´s the firt react app you make, congratulations!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes/>
   );
 }
-
 export default App;
